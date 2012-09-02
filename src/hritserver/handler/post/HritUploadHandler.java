0cb882630c4feb0318161aa83c5a3588356fe6fa
schmidda <schmidtdesmond@optusnet.com.au>
2012-09-02 19:54:02 +1000

    Updated license.

--- src/hritserver/handler/post/HritUploadHandler.java
+++ src/hritserver/handler/post/HritUploadHandler.java
@@ -1,7 +1,19 @@
-/*
- * Handle a raw upload request for any kind of file. No special processing 
- * other than to replace an existing document in the database.
+/* This file is part of hritserver.
+ *
+ *  hritserver is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  hritserver is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with hritserver.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package hritserver.handler.post;
 
 import hritserver.HritServer;
--- src/hritserver/importer/filters/MarkupSet.java
+++ src/hritserver/importer/filters/MarkupSet.java
@@ -1,7 +1,19 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/* This file is part of hritserver.
+ *
+ *  hritserver is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  hritserver is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with hritserver.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package hritserver.importer.filters;
 import java.util.ArrayList;
 import hritserver.json.corcode.Range;
