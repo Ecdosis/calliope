3a067376b7210796981cd661f35861c770095fa1
schmidda <schmidtdesmond@optusnet.com.au>
2012-09-04 05:13:05 +1000

    Updated scripts.

--- hritserver-0.1.3/add-user.sh
+++ hritserver-0.1.3/add-user.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-curl -X PUT http://admin:jabberw0cky@localhost:5984/_config/admins/admin -d '"jabberw0cky"'
+read -p "Admin password:" PASSWORD
+curl -X PUT http://localhost:5984/_config/admins/admin -d '"$PASSWORD"'
