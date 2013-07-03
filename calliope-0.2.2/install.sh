f064fa63479f97a22466708ced333f7aeca85db0
Desmond Schmidt <desmond.allan.schmidt@gmail.com>
2013-07-03 19:19:50 +1000

    fixed install script.

--- calliope-0.2.2/install.sh
+++ calliope-0.2.2/install.sh
@@ -5,12 +5,22 @@ if [ "`uname`" = "Darwin" ]; then
   LIBSUFFIX="dylib"
   JAVAC=`whereis javac | awk '{print $1;}'`
   JDKINCLUDEDIRNAME="Headers"
+  HAS_BREW=`which brew`
+  if ( -z "$HAS_BREW" ]; then
+    echo "Please install homebrew"
+    exit
+  fi
 else
   APACHE2_LOC=`which apache2`
   LIBSUFFIX="so"
   HASREADLINK=`whereis -b readlink | awk '{print $2;}'`
   JAVAC=`whereis -b javac | awk '{print $2;}'`
   JDKINCLUDEDIRNAME="include"
+fi
+HAS_GCC=`which gcc`
+if [ -z "$HAS_GCC" ]; then
+  echo "Please install gcc"
+  exit
 fi
 if [ -z "$JAVAC" ]; then
   echo "Install a jdk first!"
@@ -20,6 +30,7 @@ if [ -z "$APACHE2_LOC" ]; then
   echo "Please install apache2"
   exit
 fi
+# passed all basic sanity checks
 a2enmod proxy 1>/dev/null
 a2enmod proxy_http 1>/dev/null
 a2enmod proxy_balancer 1>/dev/null
@@ -64,10 +75,12 @@ fi
 # set up proxy
 if [ -d /etc/apache2/mods-available/ ]; then
   cp proxy.conf /etc/apache2/mods-available/
-elif grep -Fq "ProxyPass /calliope/ http://localhost:8080/calliope/ retry=1" /etc/apache2/mods-available/proxy.conf ; then 
+elif grep -Fq "ProxyPass /calliope/ http://localhost:8080/calliope/ retry=0" /etc/apache2/httpd.conf ; then 
   echo "proxy already set up"
 elif [ -e /etc/apache2/httpd.conf ]; then
    cat httpd.conf >> /etc/apache2/httpd.conf 
+else
+  echo "unrecognised apache configuration"
 fi
 echo "You must set up mongodb's admin user and then upload the test data."
 echo "You must also upload the sample data"
