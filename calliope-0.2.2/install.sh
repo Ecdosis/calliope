9b6126c745f64b1a3f4b637b91ab6c88543339b4
Desmond Schmidt <desmond.allan.schmidt@gmail.com>
2013-07-03 18:58:02 +1000

    updated installer to work on Linux at least

--- calliope-0.2.2/install.sh
+++ calliope-0.2.2/install.sh
@@ -1,15 +1,28 @@
 #!/bin/sh
 if [ "`uname`" = "Darwin" ]; then
+  APACHE2_LOC=`which httpd`
   HASREADLINK=`whereis readlink | awk '{print $1;}'`
   LIBSUFFIX="dylib"
   JAVAC=`whereis javac | awk '{print $1;}'`
   JDKINCLUDEDIRNAME="Headers"
 else
+  APACHE2_LOC=`which apache2`
   LIBSUFFIX="so"
   HASREADLINK=`whereis -b readlink | awk '{print $2;}'`
   JAVAC=`whereis -b javac | awk '{print $2;}'`
   JDKINCLUDEDIRNAME="include"
 fi
+if [ -z "$JAVAC" ]; then
+  echo "Install a jdk first!"
+  exit
+fi
+if [ -z "$APACHE2_LOC" ]; then
+  echo "Please install apache2"
+  exit
+fi
+a2enmod proxy 1>/dev/null
+a2enmod proxy_http 1>/dev/null
+a2enmod proxy_balancer 1>/dev/null
 LIB_LOC=/usr/local/lib
 getjdkinclude()
 {
@@ -30,17 +43,14 @@ if [ $USER = "root" ]; then
   if [ -d $JDKINCLUDE ]; then
     gcc -DHAVE_EXPAT_CONFIG_H -DJNI -DHAVE_MEMMOVE -Istripper/include -I$JDKINCLUDE -O0 -g3 -Wall -fPIC stripper/src/*.c -shared -o libAeseStripper.$LIBSUFFIX
     gcc -DHAVE_EXPAT_CONFIG_H -DJNI -DHAVE_MEMMOVE -Iformatter/include -Iformatter/include/AESE -Iformatter/include/STIL -I$JDKINCLUDE -O0 -g3 -Wall -fPIC formatter/src/*.c formatter/src/AESE/*.c formatter/src/STIL/*.c -shared -o libAeseFormatter.$LIBSUFFIX
-    if [ -e /usr/local/lib/libAeseStripper.$LIBSUFFIX ]; then
-      rm /usr/local/lib/libAeseStripper.$LIBSUFFIX
+    if [ -d /usr/local/lib/libAeseStripper.$LIBSUFFIX.dSYM ]; then
+      rm -rf libAeseStripper.$LIBSUFFIX.dSYM
     fi
-    if [ -e /usr/local/lib/libAeseFormatter.$LIBSUFFIX ]; then
-      rm /usr/local/lib/libAeseFormatter.$LIBSUFFIX
+    if [ -d /usr/local/lib/libAeseFormatter.$LIBSUFFIX.dSYM ]; then
+      rm -rf libAeseFormatter.$LIBSUFFIX.dSYM
     fi
-    cp libAeseStripper.$LIBSUFFIX $LIB_LOC
-    cp libAeseFormatter.$LIBSUFFIX $LIB_LOC
-    if [ ! -d "/usr/local/bin/" ]; then
-      mkdir /usr/local/bin
-    fi
+    mv libAeseStripper.$LIBSUFFIX $LIB_LOC
+    mv libAeseFormatter.$LIBSUFFIX $LIB_LOC
   else
     echo "couldn't find jdk include directory ($JDKINCLUDE)"
   fi
@@ -51,8 +61,16 @@ if [ $USER = "root" ]; then
 else
   echo "Need to be root. Did you use sudo?"
 fi
+# set up proxy
+if [ -d /etc/apache2/mods-available/ ]; then
+  cp proxy.conf /etc/apache2/mods-available/
+elif grep -Fq "ProxyPass /calliope/ http://localhost:8080/calliope/ retry=1" /etc/apache2/mods-available/proxy.conf ; then 
+  echo "proxy already set up"
+elif [ -e /etc/apache2/httpd.conf ]; then
+   cat httpd.conf >> /etc/apache2/httpd.conf 
+fi
 echo "You must set up mongodb's admin user and then upload the test data."
-echo "Ensure apache config is set up correctly"
+echo "You must also upload the sample data"
 echo "See README.txt for details"
 echo "To start the service type ./calliope-start.sh"
 echo "To stop it type ./calliope-stop.sh"
--- calliope-0.2.2/pdef-tool/rebuild.sh
+++ calliope-0.2.2/pdef-tool/rebuild.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
+install_manpage()
+{
+  manpaths=`man -w`
+  OIFS="$IFS"
+  IFS=':'
+  patharray=($manpaths)
+  IFS=$OIFS
+  i=0
+  while [ $i -lt ${#patharray[@]} ]
+  do
+    lsres=`ls ${patharray[$i]}`
+    if [[ $lsres == *man1* ]]; then
+        cp $1 ${patharray[$i]}"/man1"
+        break
+    fi
+    (( i=i+1 ))
+  done
+}
 if [ $USER = "root" ]; then
   gcc -Iinclude src/*.c -lm -o pdef-tool
   gzip -c pdef-tool.1 > pdef-tool.1.gz 
-  cp pdef-tool.1.gz /usr/local/share/man/man1/
+  install_manpage pdef-tool.1.gz
   if [ ! -d "/usr/local/bin/" ]; then
       mkdir /usr/local/bin
   fi
-  cp pdef-tool /usr/local/bin/
+  mv pdef-tool /usr/local/bin/
 else
   echo "Did you use sudo?"
 fi
