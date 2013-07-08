2e291b62cc02419107f4534d2c4efd78ff23953d
Desmond Schmidt <desmond.allan.schmidt@gmail.com>
2013-07-09 09:35:19 +1000

    Modified installer to set mongo admin user on older versions of mongo correctly

--- calliope-0.2.2/mongouser.js
+++ calliope-0.2.2/mongouser.js
@@ -1,5 +1,5 @@
 db = db.getSiblingDB('admin')
 if ( db.system.users.find({user: "admin"}).count()==0 ) 
 { 
-  db.addUser({user:"admin", pwd:"jabberw0cky", roles: ["userAdminAnyDatabase"]}) 
+  db.addUser('admin', 'jabberw0cky', 'userAdminAnyDatabase') 
 }
