25941901a90beea5a1dd6be3bd72417165801d1a
Desmond Schmidt <desmond.allan.schmidt@gmail.com>
2013-08-06 09:57:57 +1000

    fixed hh_exceptions param name

--- src/calliope/constants/Params.java
+++ src/calliope/constants/Params.java
@@ -97,7 +97,7 @@ public class Params
     public final static String UPLOAD = "UPLOAD";
     public final static String SIMILARITY = "SIMILARITY";
     public final static String DICT = "DICT";
-    public final static String HH_EXCEPTIONS = "HH_EXCEPTIONS";
+    public final static String HH_EXCEPTIONS = "hh_exceptions";
     /** default xslt stylesheet */
     public final static String XSLT_DEFAULT = "default";
     public final static String ZIP_TYPE = "zip_type";
