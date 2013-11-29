1c7aece4f1dea07bd30fcf0aec6bed3267ed074c
desmond <desmond.allan.schmidt@gmail.com>
2013-11-29 11:29:13 +1000

    Added -Xincgcto setenv.sh

--- tomcat-bin/setenv.sh
+++ tomcat-bin/setenv.sh
@@ -14,4 +14,4 @@ LIBPATH=`java -cp /usr/local/fedora/tomcat/bin/ LibPath`
 if [ `echo $LIBPATH | grep -c "/usr/local/lib" ` -eq 0 ] ; then
   LIBPATH=$LIBPATH:/usr/local/lib
 fi
-export CATALINA_OPTS="-Djava.library.path=$LIBPATH -cp .$JARPATHS"
+export CATALINA_OPTS="-Xincgc -Djava.library.path=$LIBPATH -cp .$JARPATHS"
